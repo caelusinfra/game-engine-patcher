4b88216f7fdca5be0021066f6e1ffbe6978439ed
karma <81528430+ryaxd@users.noreply.github.com>
2026-02-19 16:47:55 +0300

    fix Windows builds?
    Signed-off-by: karma <81528430+ryaxd@users.noreply.github.com>

--- src/xmake.lua
+++ src/xmake.lua
@@ -10,6 +10,19 @@ target("game-engine-patcher")
         set_basename("GameEnginePatcher")
     end
 
+    add_links(
+        "user32",
+        "ws2_32",
+        "wldap32",
+        "crypt32",
+        "normaliz",
+        "advapi32",
+        "iphlpapi",
+        "secur32"
+    )
+
     add_includedirs("../include")
 
     add_packages("vcpkg::curl", "vcpkg::curl[openssl]", "vcpkg::openssl", "vcpkg::zlib", "vcpkg::nlohmann-json", "vcpkg::pugixml")
+
+
